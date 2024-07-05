9573c499f8ff384b59f24637f26ebd3e74dc1952
devmak <khan@nochallenge.net>
2024-07-06 01:41:39 +0600

    show api data on body

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -9,11 +9,14 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  List<dynamic> imageBucketListData = [];
+
   Future<void> getData() async {
     try {
       Response response = await Dio().get(
-          "https:3//flutterapitest-8d075-default-rtdb.asia-southeast1.firebasedatabase.app/bucketlist.json");
-      print(response.data);
+          "https://flutterapitest-8d075-default-rtdb.asia-southeast1.firebasedatabase.app/bucketlist.json");
+      imageBucketListData = response.data;
+      setState(() {});
     } catch (e) {
       showDialog(
           context: context,
@@ -33,7 +36,32 @@ class _MainScreenState extends State<MainScreen> {
         title: Text("Rb Image Bucket"),
         centerTitle: true,
       ),
-      body: ElevatedButton(onPressed: getData, child: Text("Get Data")),
+      body: Column(
+        children: [
+          ElevatedButton(onPressed: getData, child: Text("Get Data")),
+          Expanded(
+            child: ListView.builder(
+                itemCount: imageBucketListData.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(imageBucketListData[index]
+                                ['image'] ??
+                            "https://scontent.fdac14-1.fna.fbcdn.net/v/t39.30808-6/428629326_3624898381084843_3442857291648613134_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=z-A-PwcH2ioQ7kNvgEQDL4c&_nc_ht=scontent.fdac14-1.fna&oh=00_AYDpxfJk64oKZbWOKgHcAQuzyhJhLB3Kl-NmTmO69RNE9w&oe=668E20C5"),
+                      ),
+                      title: Text(imageBucketListData[index]['item'] ?? "N/A"),
+                      trailing: Text(
+                        imageBucketListData[index]['cost'].toString(),
+                        style: TextStyle(fontSize: 15),
+                      ),
+                    ),
+                  );
+                }),
+          ),
+        ],
+      ),
     );
   }
 }
